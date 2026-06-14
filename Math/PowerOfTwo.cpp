618c3355dcad26f0db5dbb07910c9f49c847af5b
vivek Sharma <vivek19221922@gmail.com>
2026-06-14 06:35:15 +0530

    Update PowerOfTwo.cpp

--- Math/PowerOfTwo.cpp
+++ Math/PowerOfTwo.cpp
@@ -1,1 +1,16 @@
+/*
+Problem: Power of Two
+Platform: LeetCode
+Difficulty: Easy
 
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        return (n & (n - 1)) == 0;
+    }
+};
